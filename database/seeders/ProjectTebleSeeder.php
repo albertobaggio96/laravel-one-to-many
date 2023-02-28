f5cb42a9f796f3bfd5f2f1380b7f9e988fefe305
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-28 15:53:46 +0100

    add seed type_id to project seeder

--- database/seeders/ProjectTebleSeeder.php
+++ database/seeders/ProjectTebleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -19,12 +20,15 @@ class ProjectTebleSeeder extends Seeder
     {
         for($i = 0; $i < 60; $i++){
             $newProject = new Project();
+
+            $newProject->type_id = Type::inRandomOrder()->first()->id;
+
             $newProject->title = $faker->unique()->sentence(3);
             $newProject->slug = Str::slug($newProject->title);
             $newProject->author = "Alberto Baggio";
             $newProject->date = $faker->date();
             $newProject->preview = $faker->imageUrl(640, 640);
-            $newProject->framework = "vanilla";
+            
             $newProject->save();
         }
     }
